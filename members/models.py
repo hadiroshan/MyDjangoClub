f93e2018a72180345656ed7f28d9cd200588fdfe
hadiroshan <roshan.hadi@gmail.com>
2024-01-13 11:54:50 +0330

    Change Adr

--- members/models.py
+++ members/models.py
@@ -5,7 +5,8 @@ class listmember(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=30)
     mobile=models.CharField(max_length=12)
+    adr=models.TextField()
     def __str__(self) :
-        return str(self.id)+"\t"+self.name+"\t"+str(self.mobile)
+        return str(self.id)+"\t"+self.name+"\t"+str(self.mobile)+"\t"+self.adr
 
 # Create your models here.
